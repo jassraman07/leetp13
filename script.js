b112119f095b0187a960513c43c9afc0d9272654
jassraman07 <142134609+jassraman07@users.noreply.github.com>
2023-11-11 11:06:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+//Question reverse a string but i am unable to solve this problem still uoloading as a p13//
 
+
+
+// const str = "hello";
+// const reversed = reverseStr(str);
+
+// console.log(reversed); 
  function reverseString(str) {
-    return str.split("").reverse().join("");;
+    return str.split("").reverse().join("");
 }
 
 console.log(string_reverse("hello"));
